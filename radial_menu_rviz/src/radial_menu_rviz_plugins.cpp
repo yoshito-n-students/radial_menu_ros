df61918e3f480407f0f134cdaacf740cff43337f
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2020-08-03 07:04:04 +0900

    apply clang-format

--- radial_menu_rviz/src/radial_menu_rviz_plugins.cpp
+++ radial_menu_rviz/src/radial_menu_rviz_plugins.cpp
@@ -1,6 +1,6 @@
 #include <pluginlib/class_list_macros.h>
-#include <radial_menu_rviz/radial_display.hpp>
 #include <radial_menu_rviz/horizontal_display.hpp>
+#include <radial_menu_rviz/radial_display.hpp>
 #include <rviz/display.h>
 
 PLUGINLIB_EXPORT_CLASS(radial_menu_rviz::RadialDisplay, rviz::Display)
